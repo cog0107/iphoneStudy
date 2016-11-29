ddbd58897b5d31734ebbe33e329ffd41a6f39c6b
CHLOE <chloe@chloeui-macbook-pro.local>
2016-11-29 14:03:28 +0900

    study

--- login/ViewController.m
+++ login/ViewController.m
@@ -14,6 +14,12 @@
 
 @implementation ViewController
 
+
+- (IBAction)username:(id)sender {
+}
+
+
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
